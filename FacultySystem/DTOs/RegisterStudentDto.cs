d5e6fb098d9025aae22d11ddffba431c8d77e651
Abdelaziz <=>
2025-03-03 14:58:33 +0200

    update rigisterStudentDtoCopy.cs

--- FacultySystem/DTOs/RegisterStudentDto.cs
+++ FacultySystem/DTOs/RegisterStudentDto.cs
@@ -10,6 +10,9 @@ namespace FacultySystem.DTOs
         [Required, EmailAddress]
         public string Email { get; set; }
 
+        // commment to test
+        //public string Email { get; set; }
+
         [Required, MinLength(8)]
         public string Password { get; set; }
 
